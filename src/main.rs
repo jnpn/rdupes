91cb38c4bfb2a40904abcd9a75a909ddd812cf66
Johan PONIN <johan.ponin.pro@gmail.com>
2021-04-13 01:13:06 +0200

    [fat] toyed with hashmaps and borrows,
    FIXED md5 (through HEXUPPER)
    FIXED walkmap

--- src/main.rs
+++ src/main.rs
@@ -8,40 +8,21 @@ use std::collections::HashMap;
 use md5::{Md5, Digest};
 use glob::glob;
 
-#[derive(Debug, Clone, Copy)]
-struct Vec3 {
-    x: f64,
-    y: f64,
-    z: f64
-}
-
-fn add(u:Vec3, v:Vec3) -> Vec3 {
-    Vec3 {
-	x: u.x + v.x,
-	y: u.y + v.y,
-	z: u.z + v.z,
-    }
-
-}
-
-use std::fmt;
+use data_encoding::HEXUPPER;
 
 #[derive(Debug)]
-struct Person {
-    name: String,
-    age: u32,
+struct Pair<A> {
+    a:A,
+    b:A,
 }
 
-impl fmt::Display for Person {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-	write!(f, "<Person {} {}>", self.name, self.age)
-    }
+fn g() {
+    let p = Pair { a:0,b:1 }; // { a:0, b:1 };
+    println!("({},{})", p.a, p.b);
 }
 
-impl Drop for Person {
-    fn drop(&mut self) {
-	println!("{}--", self);
-    }
+fn f(x: &u8, f: fn(u8) -> u8) -> u8 {
+    return f(*x + *x);
 }
 
 fn gen_hm(c:u32) -> HashMap<u32,String> {
@@ -51,38 +32,6 @@ fn gen_hm(c:u32) -> HashMap<u32,String> {
     }
     return m;
 }
-fn gen_person (age:u32) {
-    let _p:Person = Person {
-	name: "Jack".to_string(),
-	age: age
-    };
-}
-
-
-// fn main() {
-//     let d = "/tmp/fdt/";
-//     let files = WalkDir::new(d)
-// 	.into_iter()
-// 	.filter_map(|e| e.ok())
-// 	.map(|e| {
-// 	    let f = e.clone();
-// 	    let p = e.path().clone();
-// 	    let m = fs::metadata(p);
-// 	    let d = Utc::now();
-// 	    let h = hash(p);
-// 	    return (f, m, d, h)
-// 	})
-// 	.collect::<Vec<_>>();
-//     // .collect::<HashMap<WalkDir::DirEntry, &str>>();
-//     for (e,m,d,h) in &files {
-// 	let me = match m {
-// 	    Err(_) => "?",
-// 	    Ok(m) => if m.is_dir() { "directory" } else { "file" }
-// 	};
-// 	println!("{} : {}\t{}\t{:x}", e.path().display(), me, d, h);
-//     }
-//     println!("found {} file(s)", files.len());
-// }
 
 fn md5(p:&Path) -> (String,String) {
     let mut m = Md5::new();
@@ -95,84 +44,104 @@ fn md5(p:&Path) -> (String,String) {
 	None => "?",
     };
     match c {
-	// Ok(c) => m.update(String::from_utf8_lossy(c)),
 	Ok(b) => m.update(b.as_bytes()),
 	Err(_) => ()
     }
-    // let d = m.finalize().as_slice().iter().collect::<Vec<&u8>>();
-    // for u in m.finalize().as_slice().iter() {
-    // 	println!("{}", u);
-    // }
-    // let s = m.finalize().as_slice(); // .iter().collect::<Vec<&u8>>();
-    // let t = String::from(s);
 
-    // let s = m.finalize().as_slice().iter()
-    // 	.collect::<Vec<&u8>>();
-
-    // let hs = String::from_utf8_lossy(d);
-    // return (String::from("0x01393092"), f.to_string());
-    return (f.to_string(), format!("{:?}", m.finalize()[..].into_iter().collect::<Vec<&u8>>()));
+    let a = &m.finalize()[..];
+    let b = a.into_iter().collect::<Vec<&u8>>();
+    let mut r:[u8; 16] = [0 ; 16];
+    let mut i = 0;
+    for u in &b {
+	r[i] = **u;
+	i += 1;
+    }
+    let s = String::from(HEXUPPER.encode(&r));
+    return (f.to_string(), s);
 }
 
-
 fn walkmap(root:String, keyer:fn(String) -> String) -> HashMap<String, Vec<String>> {
-    // [(str, path)]
-    let files = WalkDir::new(root)
+    // Vec<(String,  String)>
+    let files: Vec<(String, String)> = WalkDir::new(root)
 	.into_iter()
-	.filter_map(|e| e.ok())
+	.filter_map(Result::ok)
 	.map(|e| md5(e.path()))
 	.collect::<Vec<_>>();
-    println!("{:?}", files);
-    let mut m = HashMap::new();
-    m.insert(keyer("@@@".to_string()), vec!["foo".to_string(), "bar".to_string()]);
+    let mut m:HashMap<String, Vec<String>> = HashMap::new();
+
+    // m.insert(keyer("@@@".to_string()), vec!["foo".to_string(), "bar".to_string()]);
+    println!("{}", keyer("@@@".to_string()));
+    // for (n,v) in &files {
+    // 	println!("{}:-{:?}", n, v);
+    // }
+
+    // let mut n:HashMap<String, u32> = HashMap::new();
+    // for (_,h) in files {
+    // 	if n.contains_key(&h) {
+    // 	    n.entry(h).or_insert(100);
+    // 	} else {
+    // 	    n.insert(h, 1);
+    // 	}
+    // }
+    
+    for (f,h) in files {
+	match m.get_mut(&h) {
+	    Some(v) => v.push(f),
+	    None => { m.entry(h).or_insert(vec![f]); },
+	}
+    }
+
     return m;
 }
 
 fn main() {
-    let a = Vec3 { x:1.0, y:1.0, z:1.0 };
-    println!("[vec]\t{:?}", a);
-    let b = a;
-    println!("[vadd]\t{:?}", add(a,b));
-
-    let p = Person {
-	name: String::from("Alan"),
-	age: 107
-    };
-    println!("[person]\t{:?}", p);
-    for i in 50..100 {
-	gen_person(i);
-    }
-    println!("[nop]\tyolo {}", "!".to_string());
-
     let m = gen_hm(12);
     println!("[gen_hm]\t{:?}", m);
+    println!("----");
 
-    let mut m:HashMap<String, Vec<String>> = HashMap::new();
-    m.insert("0x2943083".to_string(), vec!["/a/b/c".to_string(), "/b/c/d".to_string()]);
-    m.insert("0x2940003".to_string(), vec!["/a/b".to_string(), "/b/c".to_string()]);
-    m.insert("0x2000083".to_string(), vec!["/a/c".to_string(), "/d".to_string()]);
+    let (a,b) = md5(std::path::Path::new("/home/noob/.aliases"));
+    println!("[1hash] {} # {}", a, b);
+    println!("----");
 
-    let k = "0x2000083";
-    let s = match m.get(k) {
-	Some(v) => format!("{} -> {:?}", k, v),
-	None => format!("nil")
-    };
-    println!("[map]\t{:?}", m);
-    println!("[match]\t{}", s);
+    let root = "/home/noob/bin/*";
 
-	// let (a,b) = md5(std::path::Path::new("/home/noob/.aliases"));
-	// println!("{} # {}", b, a);
-
-    // for e in glob("/tmp/fdt/*").expect("glob failed") {
-    for e in glob("/home/noob/bin/*").expect("glob failed") {
+    for e in glob(root).expect("glob failed") {
 	match e {
-	    Ok(f) => {
-		let (a,b) = md5(std::path::Path::new(&f.display().to_string()));
-		println!("{} # {}", b, a);
+	    Ok(p) => {
+		let (a,b) = md5(p.as_path());
+		println!("{} # {}", a, b);
 	    },
 	    Err(e) => println!("{:?}", e)
 	}
     }
+
+    // match glob(root) {
+    // 	Ok(paths) => {
+    // 	    for p in paths {
+    // 		match p {
+    // 		    Ok(pb) => {
+    // 		    	let (a,b) = md5(pb.as_path());
+    // 			println!("{} {}", a, b);
+    // 		    },
+    // 		    Err(f) => println!("{:?}", f)
+    // 		}
+    // 	    };
+    // 	}
+    // 	Err(e) => println!("{:?}", e),
+    // };
+    
+    println!("----");
     println!("[WALK]");
-    walkmap("/tmp/fdt/".to_string(), |s| s);
+    for (k,v) in walkmap("/home/noob/bin/".to_string(), |s| s) {
+	println!("{} -> {:?}", k, v);
+    }
+
+    g();
+
+    let x:u8 = 23;
+    let y = f(&x, |x| x+x);
+    println!("{} -> {}", x, y);
+
+    println!("----");
+    println!("bye.");
 }
